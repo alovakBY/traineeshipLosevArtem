7e6e1e07d54abd50d5ef9eb8d4334fe40dd9659c
alovakBY <alovakby@gmail.com>
2021-12-20 17:32:14 +0300

    homeWork3

--- homeWork3/helloWorld.js
+++ homeWork3/helloWorld.js
@@ -3,13 +3,14 @@
 // Задача номер 1
 
 const btn1 = document.querySelector(".btn1");
+let idSetTimeout;
 
 btn1.addEventListener("click", helloWorld);
 
 function helloWorld() {
-  setTimeout(() => {
-    console.log("Hello world");
-    btn1.addEventListener("click", helloWorld);
-  }, 5000);
-  btn1.removeEventListener("click", helloWorld);
+    clearTimeout(idSetTimeout);
+    idSetTimeout = setTimeout(() => {
+        console.log("Hello world");
+        btn1.addEventListener("click", helloWorld);
+    }, 5000);
 }
