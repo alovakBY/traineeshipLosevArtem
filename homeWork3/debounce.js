77d243d98f64527513ffc231d56591dd33b17ef0
alovakBY <alovakby@gmail.com>
2021-12-28 19:56:36 +0300

    commit

--- homeWork3/debounce.js
+++ homeWork3/debounce.js
@@ -1,18 +1,21 @@
-"use strict";
+// "use strict";
 
 function debounce(func, delay) {
-    let cooldown = false;
-    let id;
-    return function () {
-        if (cooldown) {
-            clearTimeout(id);
-        }
-        cooldown = true;
-        id = setTimeout(() => {
-            func.call(this);
-            cooldown = false;
-        }, delay);
-    };
+  let cooldown = false;
+  let id;
+  console.log(this);
+  return function () {
+    console.log(this);
+    if (cooldown) {
+      clearTimeout(id);
+    }
+    cooldown = true;
+    id = setTimeout(() => {
+      console.log(this);
+      func.call(this);
+      cooldown = false;
+    }, delay);
+  };
 }
 
 // Задача номер 1
@@ -20,17 +23,17 @@ function debounce(func, delay) {
 const btn1 = document.querySelector(".btn1");
 
 function helloWorld() {
-    console.log("Hello world");
+  console.log("Hello world");
 }
 
-btn1.addEventListener("click", debounce(helloWorld, 5000));
+btn1.addEventListener("click", debounce(helloWorld, 1000));
 
 // Задача номер 4
 
-const input = document.querySelector(".input");
+// const input = document.querySelector(".input");
 
-function getInputText() {
-    console.log(this.value);
-}
+// function getInputText() {
+//   console.log(this.value);
+// }
 
-input.addEventListener("input", debounce(getInputText, 1000));
+// input.addEventListener("input", debounce(getInputText, 1000));
